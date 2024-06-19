91eb16e40b8060757801a87bd2812270d1d39f07
Cauan Diniz <cauanrodiniz@gmail.com>
2024-06-19 19:12:12 -0300

    feat: add link to direct section

--- src/components/common/Topbar/Topbar.tsx
+++ src/components/common/Topbar/Topbar.tsx
@@ -2,6 +2,7 @@ import { Send } from "lucide-react"
 import Logo from "../Logo"
 import UserDropdown from "../UserDropdown/UserDropdown";
 import useAuthContext from "@/hooks/useAuthContext";
+import Link from "next/link";
 
 const Topbar = () => {
   const { user } = useAuthContext();
@@ -9,7 +10,9 @@ const Topbar = () => {
     <div className="w-full top-0 sticky p-4 flex justify-between items-center lg:hidden">
       <UserDropdown avatarUrl={ user?.avatarUrl ?? "" } />
       <Logo />
-      <Send className="text-white" />
+      <Link href={"/user/directs"}>
+        <Send className="text-white cursor-pointer hover:text-red-600" />
+      </Link>
     </div>
   )
 }
